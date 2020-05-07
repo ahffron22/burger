7dd23bdaaa4956852f60cc8161cde7ef6aea50bc
ahffron <ahffron22@gmail.com>
2020-05-06 23:22:00 -0600

    can't figure out why this doesn't work

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('This Burger', 69);
+INSERT INTO burgers (burger_name, devoured) VALUES ('That Burger', 69);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Burger', 69);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Other Burger', 69);
